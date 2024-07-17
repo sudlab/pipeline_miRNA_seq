ca3924ab4090935c07711cea90e1d91801ed5c32
bo1cv <charlotte.vandermeulen8@gmail.com>
2024-07-17 10:14:33 +0100

    update pipeline to make it more comprehensive

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# pipeline_miRNA_seq_favour
-miRseq and RNAseq pipeline analysis for Favour's data
+# pipeline_miRNA_seq
+
+This pipeline performs miRNAseq mapping with STAR and counts mapped reads to
+a reference genome.
+
+
+## Requirements
+
+On top of the default CGAT setup, the pipeline requires the following.
+
+Software:
+    python (v3.8.12 with pysam v0.17.0 when built)
+    STAR ()
+    featurecounts
+    multiqc
+
+
+## Configuration
+
+The pipeline requires a configured :file: pipeline.yml file.
+
+Make a directory with your project name. Configure the pipeline with 
+
+python [path_to_repo]/pipeline_motif_analysis.py config
+
+A pipeline.log and pipeline.yml file(s) will be added to your new directory.
+Modify the pipeline.yml according to your project.
+
+
+## Inputs
+
+- Trimmed unpaired fasta files ending in .fastq.gz
+- a STAR genome directory, pat hto be specified in pipeline.yml
+- a GTF adapted for featureCounts for miRNAs
+
+
+## Run pipeline
+
+Run the pipeline with python [path_to_repo]/pipeline_miRNA_seq.py make full -v5.
+
+
+## Ouputs
---
+++ pipeline_miRNA_seq.py
@@ -0,0 +1,110 @@
+import sys
+import os
+import sqlite3
+
+from cgatcore import pipeline as P
+import cgat.GTF as GTF
+import cgatcore.iotools as IOTools
+from ruffus import *
+
+#Load config file options
+PARAMS = P.get_parameters(
+    ["%s/pipeline.yml" % os.path.splitext(__file__)[0],
+     "../pipeline.yml",
+     "pipeline.yml"])
+
+#First roung mapping
+PARAMS = P.get_parameters("pipeline.yml")
+
+@subdivide("*.fastq.gz",
+           regex("(.+).fastq.gz"),
+           [r"star.dir/\1.Aligned.sortedByCoord.out.bam",
+           r"star.dir/\1.Unmapped.out.mate1"])
+def star_mapping(infile, outfiles):
+    job_memory = "8G"
+    job_threads = 4
+    mismax = PARAMS["star_max_mismatch"]
+    multi = PARAMS["star_max_multi"]
+    cover = PARAMS["star_mis_cover"]
+    genomeDir = PARAMS["star_genome"]
+    length = PARAMS["star_min_length_match"]
+    outname = P.snip(outfiles[0], "Aligned.sortedByCoord.out.bam")
+    '''STAR mapping'''
+    statement="""
+    STAR --runThreadN 4
+    --genomeDir %(genomeDir)s
+    --readFilesIn %(infile)s
+    --readFilesCommand zcat
+    --outSAMtype BAM SortedByCoordinate
+    --quantMode TranscriptomeSAM GeneCounts
+    --outReadsUnmapped Fastx
+    --outFileNamePrefix %(outname)s
+    --outFilterMultimapScoreRange 0
+    --outFilterMultimapNmax %(multi)s
+    --outFilterScoreMinOverLread 0
+    --outFilterMatchNminOverLread 0
+    --outFilterMatchNmin %(length)s
+    --outFilterMismatchNmax %(mismax)s
+    --alignSJDBoverhangMin 1000
+    --alignEndsType EndToEnd
+    --alignIntronMax 1
+    --outMultimapperOrder Random
+    --outFilterMismatchNoverLmax %(cover)s
+    """
+    P.run(statement)
+
+
+@collate(star_mapping,
+         regex("(star.dir)/(.+).Aligned.sortedByCoord.out.bam"),
+         r"\1/multiqc_report.html")
+def multiQCstar(infile, outfile):
+    """Run multiqc on star mapping"""
+    job_memory = "2G"
+    statement = '''
+    "export LANG=en_GB.UTF-8 && "
+    "export LC_ALL=en_GB.UTF-8 && "
+	"multiqc -f -n %(outfile)s --no-data-dir star.dir/ "
+    '''
+    P.run(statement)
+
+
+@transform(star_mapping,
+           regex("(.+).dir/(.+).Aligned.sortedByCoord.out.bam"),
+           r"counts.dir/\2_counts.txt")
+def featureCounts(infile, outfile):
+    """running featureCounts on mapped reads"""
+    job_memory = "4G"
+    job_threads = 4
+    anno = PARAMS["feacturecouts_gtf_gff"]
+    more_params = PARAMS["featurecouts_options"]
+    #theinfile = str(infile)+""
+    '''featureCounts'''
+    statement="""
+    featureCounts -a %(anno)s
+    -T 4
+    %(more_params)s
+    -o %(outfile)s %(infile)s
+    """
+    P.run(statement)
+
+
+@collate(featureCounts,
+         regex("(counts.dir)/(.+).txt"),
+         r"\1/multiqc_report.html")
+def multiQCfc(infile, outfile):
+    """Run multiqc on star mapping"""
+    job_memory = "2G"
+    statement = '''
+    "export LANG=en_GB.UTF-8 && "
+    "export LC_ALL=en_GB.UTF-8 && "
+	"multiqc -f -n %(outfile)s --no-data-dir counts.dir/ "
+    '''
+    P.run(statement)
+
+
+@follows(featureCounts, multiQCfc, multiQCstar)
+def full():
+    ''' Later alligator '''
+    pass
+
+P.main()
